604ad3a5663618890b8dd82696c842559f3857a6
aquispearr <aquispearr@unsa.edu.com>
2024-05-25 18:52:41 -0500

    2A: Completed

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -1,2 +1,9 @@
 from interpreter import draw
 from chessPictures import *
+
+caballo1 = Picture(KNIGHT)
+caballo2 = caballo1.negative()
+fila1 = caballo1.join(caballo2)
+fila2 = fila1.negative()
+tablero = fila1.up(fila2)
+draw(tablero)
