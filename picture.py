6c0c18f901298acb3339ed01f9e288fec6d3d86d
aquispearr <aquispearr@unsa.edu.com>
2024-05-25 16:39:15 -0500

    Negative funtion: Implentacion de metodo usando invColor()

--- picture.py
+++ picture.py
@@ -23,8 +23,15 @@ class Picture:
     return Picture(None)
 
   def negative(self):
-    """ Devuelve un negativo de la imagen """
-    return Picture(None)
+       """ Devuelve un negativo de la imagen """
+      negativo = []
+      for value in self.img:
+          filaInvertida = ""
+          for character in value:
+              inverted_character = self._invColor(character)
+              filaInvertida += inverted_character
+          negativo.append(filaInvertida)
+      return Picture(negativo)
 
   def join(self, p):
     """ Devuelve una nueva figura poniendo la figura del argumento 
