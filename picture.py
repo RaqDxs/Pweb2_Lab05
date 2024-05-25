4f645fef50e5e682060ed27375d539ac1702afbf
aquispearr <aquispearr@unsa.edu.com>
2024-05-25 18:43:43 -0500

    Rotate funtion: Creación de una nueva imagen con las filas de la imagen original colocadas en orden inverso y con las columnas intercambiadas

--- picture.py
+++ picture.py
@@ -94,5 +94,12 @@ class Picture:
 
 # ToDo
   def rotate(self):
-    return Picture(None)
+    b = []
+    lenSelf = len(self.img)
+    for i in range(lenSelf):
+      a = ""
+      for value in self.img:
+        a += value[lenSelf - 1 - i]
+      b.append(a)
+    return Picture(b)
 
