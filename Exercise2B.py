b101641548aa48bef2052f5f91419e7a55167228
aquispearr <aquispearr@unsa.edu.com>
2024-05-25 19:01:42 -0500

    2B: Corregimos error y encapsulamos fila1 y fila2 en objetos picture

--- Exercise2B.py
+++ Exercise2B.py
@@ -5,5 +5,9 @@ caballo1 = Picture(KNIGHT)
 caballo2 = caballo1.negative()
 fila1 = caballo1.join(caballo2)
 fila2 = fila1.verticalMirror()
-tablero = fila1.up(fila2)
+
+fila1_picture = Picture(fila1)
+fila2_picture = Picture(fila2)
+
+tablero = fila1_picture.up(fila2_picture)
 draw(tablero)
